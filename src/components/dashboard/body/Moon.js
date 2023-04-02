eb20dac0029340ff319779b5e85a7706db2f5ca0
jakubnowygoot <jakubsiepiela@gmail.com>
2023-04-02 13:04:56 +0200

    Get day logic

--- src/components/dashboard/body/Moon.js
+++ src/components/dashboard/body/Moon.js
@@ -22,7 +22,7 @@ function Moon() {
   const [isLoaded, setIsLoaded] = useState(false);
 
   const date = new Date();
-  const day = `0${date.getUTCDate()}`.slice(-2);
+  const day = `0${date.getUTCDate() + 1}`.slice(-2);
   const month = `0${date.getMonth() + 1}`.slice(-2);
   const year = date.getFullYear();
   const dateWithDay = date.toDateString();

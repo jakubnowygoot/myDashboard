41f4cd05d11fd856d46668be25f249ad76635830
jakubnowygoot <jakubsiepiela@gmail.com>
2023-07-26 18:09:24 +0200

    Update Calendar.js

--- src/components/dashboard/body/calendar/Calendar.js
+++ src/components/dashboard/body/calendar/Calendar.js
@@ -1,6 +1,3 @@
-// import { Menu, Transition } from "@headlessui/react";
-// import { DotsVerticalIcon } from "@heroicons/react/outline";
-import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/solid";
 import {
   add,
   eachDayOfInterval,
@@ -86,92 +83,117 @@ function Calendar() {
   }
 
   return (
-    <div className="rounded border-white bg-white dark:border-gray-700 border-2 h-96 dark:bg-gray-800">
+    <div className="rounded border-white bg-white dark:border-gray-700 border-2 h-auto dark:bg-gray-800">
       <div className="grid xxs:block flex-col justify-center items-center bg-white rounded p-4 h-full w-full dark:bg-gray-800 justify-between">
-        <div className="">
-          <div className="md:grid md:divide-x md:divide-gray-200">
-            <div className="">
-              <div className="flex items-center">
-                <h2 className="flex-auto font-semibold text-gray-900">
-                  {format(firstDayCurrentMonth, "MMMM yyyy")}
-                </h2>
-                <button
-                  type="button"
-                  onClick={previousMonth}
-                  className="-my-1.5 flex flex-none items-center justify-center p-1.5 text-gray-400 hover:text-gray-500"
+        <div className="md:grid md:divide-x md:divide-gray-200">
+          <div>
+            <h1 className="text-3xl font-bold pb-2 dark:text-gray-300 text-center justify-center">
+              Calendar
+            </h1>
+            <div className="flex items-center">
+              <h2 className="flex-auto font-semibold text-gray-900">
+                {format(firstDayCurrentMonth, "MMMM yyyy")}
+              </h2>
+              <button
+                type="button"
+                onClick={previousMonth}
+                className="-my-1.5 flex flex-none items-center justify-center p-1.5 text-gray-400 hover:text-gray-500"
+              >
+                <span className="sr-only">Previous month</span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-5 h-5"
                 >
-                  <span className="sr-only">Previous month</span>
-                  <ChevronLeftIcon className="w-5 h-5" aria-hidden="true" />
-                </button>
-                <button
-                  onClick={nextMonth}
-                  type="button"
-                  className="-my-1.5 -mr-1.5 ml-2 flex flex-none items-center justify-center p-1.5 text-gray-400 hover:text-gray-500"
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M15.75 19.5L8.25 12l7.5-7.5"
+                  />
+                </svg>
+              </button>
+              <button
+                onClick={nextMonth}
+                type="button"
+                className="-my-1.5 -mr-1.5 ml-2 flex flex-none items-center justify-center p-1.5 text-gray-400 hover:text-gray-500"
+              >
+                <span className="sr-only">Next month</span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-5 h-5"
                 >
-                  <span className="sr-only">Next month</span>
-                  <ChevronRightIcon className="w-5 h-5" aria-hidden="true" />
-                </button>
-              </div>
-              <div className="grid grid-cols-7 mt-10 text-xs leading-6 text-center text-gray-500">
-                <div>S</div>
-                <div>M</div>
-                <div>T</div>
-                <div>W</div>
-                <div>T</div>
-                <div>F</div>
-                <div>S</div>
-              </div>
-              <div className="grid grid-cols-7 mt-2 text-sm">
-                {days.map((day, dayIdx) => (
-                  <div
-                    key={day.toString()}
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M8.25 4.5l7.5 7.5-7.5 7.5"
+                  />
+                </svg>{" "}
+              </button>
+            </div>
+            <div className="grid grid-cols-7 mt-2.5 text-xs leading-6 text-center text-gray-500">
+              <div>S</div>
+              <div>M</div>
+              <div>T</div>
+              <div>W</div>
+              <div>T</div>
+              <div>F</div>
+              <div>S</div>
+            </div>
+            <div className="grid grid-cols-7 mt-2 text-sm">
+              {days.map((day, dayIdx) => (
+                <div
+                  key={day.toString()}
+                  className={classNames(
+                    // eslint-disable-next-line no-use-before-define
+                    dayIdx === 0 && colStartClasses[getDay(day)],
+                    "py-1.5"
+                  )}
+                >
+                  <button
+                    type="button"
+                    onClick={() => setSelectedDay(day)}
                     className={classNames(
-                      // eslint-disable-next-line no-use-before-define
-                      dayIdx === 0 && colStartClasses[getDay(day)],
-                      "py-1.5"
+                      isEqual(day, selectedDay) && "text-white",
+                      !isEqual(day, selectedDay) &&
+                        isToday(day) &&
+                        "text-red-500",
+                      !isEqual(day, selectedDay) &&
+                        !isToday(day) &&
+                        isSameMonth(day, firstDayCurrentMonth) &&
+                        "text-gray-900",
+                      !isEqual(day, selectedDay) &&
+                        !isToday(day) &&
+                        !isSameMonth(day, firstDayCurrentMonth) &&
+                        "text-gray-400",
+                      isEqual(day, selectedDay) && isToday(day) && "bg-red-500",
+                      isEqual(day, selectedDay) &&
+                        !isToday(day) &&
+                        "bg-gray-900",
+                      !isEqual(day, selectedDay) && "hover:bg-gray-200",
+                      (isEqual(day, selectedDay) || isToday(day)) &&
+                        "font-semibold",
+                      "mx-auto flex h-8 w-8 items-center justify-center rounded-full"
                     )}
                   >
-                    <button
-                      type="button"
-                      onClick={() => setSelectedDay(day)}
-                      className={classNames(
-                        isEqual(day, selectedDay) && "text-white",
-                        !isEqual(day, selectedDay) &&
-                          isToday(day) &&
-                          "text-red-500",
-                        !isEqual(day, selectedDay) &&
-                          !isToday(day) &&
-                          isSameMonth(day, firstDayCurrentMonth) &&
-                          "text-gray-900",
-                        !isEqual(day, selectedDay) &&
-                          !isToday(day) &&
-                          !isSameMonth(day, firstDayCurrentMonth) &&
-                          "text-gray-400",
-                        isEqual(day, selectedDay) &&
-                          isToday(day) &&
-                          "bg-red-500",
-                        isEqual(day, selectedDay) &&
-                          !isToday(day) &&
-                          "bg-gray-900",
-                        !isEqual(day, selectedDay) && "hover:bg-gray-200",
-                        (isEqual(day, selectedDay) || isToday(day)) &&
-                          "font-semibold",
-                        "mx-auto flex h-8 w-8 items-center justify-center rounded-full"
-                      )}
-                    >
-                      <time dateTime={format(day, "yyyy-MM-dd")}>
-                        {format(day, "d")}
-                      </time>
-                    </button>
+                    <time dateTime={format(day, "yyyy-MM-dd")}>
+                      {format(day, "d")}
+                    </time>
+                  </button>
 
-                    <div className="w-1 h-1 mx-auto mt-1">
-                      {meetings.some((meeting) =>
-                        isSameDay(parseISO(meeting.startDatetime), day)
-                      ) && <div className="w-1 h-1 rounded-full bg-sky-500" />}
-                    </div>
+                  <div className="w-1 h-1 mx-auto mt-1">
+                    {meetings.some((meeting) =>
+                      isSameDay(parseISO(meeting.startDatetime), day)
+                    ) && <div className="w-1 h-1 rounded-full bg-sky-500" />}
                   </div>
-                ))}
-              </div>
+                </div>
+              ))}
             </div>
           </div>
         </div>
@@ -179,6 +201,7 @@ function Calendar() {
     </div>
   );
 }
+
 //
 // function Meeting({ meeting }) {
 //   const startDateTime = parseISO(meeting.startDatetime);

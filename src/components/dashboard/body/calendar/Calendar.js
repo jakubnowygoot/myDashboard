b98fca6f71edadf5bdb4a848e9df4a60474e82b5
jakubnowygoot <jakubsiepiela@gmail.com>
2023-07-27 19:12:57 +0200

    Update Calendar.js

--- src/components/dashboard/body/calendar/Calendar.js
+++ src/components/dashboard/body/calendar/Calendar.js
@@ -1,3 +1,6 @@
+import { Menu, Transition } from "@headlessui/react";
+import { DotsVerticalIcon } from "@heroicons/react/outline";
+
 import {
   add,
   eachDayOfInterval,
@@ -12,7 +15,7 @@ import {
   parseISO,
   startOfToday,
 } from "date-fns";
-import { useState } from "react";
+import { Fragment, useState } from "react";
 
 const meetings = [
   {
@@ -90,6 +93,10 @@ function Calendar() {
     const firstDayNextMonth = add(firstDayCurrentMonth, { months: 1 });
     setCurrentMonth(format(firstDayNextMonth, "MMM-yyyy"));
   }
+
+  const selectedDayMeetings = meetings.filter((meeting) =>
+    isSameDay(parseISO(meeting.startDatetime), selectedDay)
+  );
 
   return (
     <div className="rounded border-white bg-white dark:border-gray-700 border-2 h-96 dark:bg-gray-800">
@@ -189,7 +196,6 @@ function Calendar() {
                       {format(day, "d")}
                     </time>
                   </button>
-
                   <div className="w-1 h-1 mx-auto mt-1">
                     {meetings.some((meeting) =>
                       isSameDay(parseISO(meeting.startDatetime), day)
@@ -199,90 +205,105 @@ function Calendar() {
               ))}
             </div>
           </div>
+          <section className="mt-12 md:mt-0 md:pl-14">
+            <h2 className="font-semibold text-gray-900">
+              Schedule for{" "}
+              <time dateTime={format(selectedDay, "yyyy-MM-dd")}>
+                {format(selectedDay, "MMM dd, yyy")}
+              </time>
+            </h2>
+            <ol className="mt-4 space-y-1 text-sm leading-6 text-gray-500">
+              {selectedDayMeetings.length > 0 ? (
+                selectedDayMeetings.map((meeting) => (
+                  <Meeting meeting={meeting} key={meeting.id} />
+                ))
+              ) : (
+                <p>No meetings for today.</p>
+              )}
+            </ol>
+          </section>
         </div>
       </div>
     </div>
   );
 }
 
-//
-// function Meeting({ meeting }) {
-//   const startDateTime = parseISO(meeting.startDatetime);
-//   const endDateTime = parseISO(meeting.endDatetime);
-//
-//   return (
-//     <li className="flex items-center px-4 py-2 space-x-4 group rounded-xl focus-within:bg-gray-100 hover:bg-gray-100">
-//       <img
-//         src={meeting.imageUrl}
-//         alt=""
-//         className="flex-none w-10 h-10 rounded-full"
-//       />
-//       <div className="flex-auto">
-//         <p className="text-gray-900">{meeting.name}</p>
-//         <p className="mt-0.5">
-//           <time dateTime={meeting.startDatetime}>
-//             {format(startDateTime, "h:mm a")}
-//           </time>{" "}
-//           -{" "}
-//           <time dateTime={meeting.endDatetime}>
-//             {format(endDateTime, "h:mm a")}
-//           </time>
-//         </p>
-//       </div>
-//       <Menu
-//         as="div"
-//         className="relative opacity-0 focus-within:opacity-100 group-hover:opacity-100"
-//       >
-//         <div>
-//           <Menu.Button className="-m-2 flex items-center rounded-full p-1.5 text-gray-500 hover:text-gray-600">
-//             <span className="sr-only">Open options</span>
-//             <DotsVerticalIcon className="w-6 h-6" aria-hidden="true" />
-//           </Menu.Button>
-//         </div>
-//
-//         <Transition
-//           as={Fragment}
-//           enter="transition ease-out duration-100"
-//           enterFrom="transform opacity-0 scale-95"
-//           enterTo="transform opacity-100 scale-100"
-//           leave="transition ease-in duration-75"
-//           leaveFrom="transform opacity-100 scale-100"
-//           leaveTo="transform opacity-0 scale-95"
-//         >
-//           <Menu.Items className="absolute right-0 z-10 mt-2 origin-top-right bg-white rounded-md shadow-lg w-36 ring-1 ring-black ring-opacity-5 focus:outline-none">
-//             <div className="py-1">
-//               <Menu.Item>
-//                 {({ active }) => (
-//                   <a
-//                     href="#"
-//                     className={classNames(
-//                       active ? "bg-gray-100 text-gray-900" : "text-gray-700",
-//                       "block px-4 py-2 text-sm"
-//                     )}
-//                   >
-//                     Edit
-//                   </a>
-//                 )}
-//               </Menu.Item>
-//               <Menu.Item>
-//                 {({ active }) => (
-//                   <a
-//                     href="#"
-//                     className={classNames(
-//                       active ? "bg-gray-100 text-gray-900" : "text-gray-700",
-//                       "block px-4 py-2 text-sm"
-//                     )}
-//                   >
-//                     Cancel
-//                   </a>
-//                 )}
-//               </Menu.Item>
-//             </div>
-//           </Menu.Items>
-//         </Transition>
-//       </Menu>
-//     </li>
-//   );
-// }
+function Meeting({ meeting }) {
+  const startDateTime = parseISO(meeting.startDatetime);
+  const endDateTime = parseISO(meeting.endDatetime);
+
+  return (
+    <li className="flex items-center px-4 py-2 space-x-4 group rounded-xl focus-within:bg-gray-100 hover:bg-gray-100">
+      <img
+        src={meeting.imageUrl}
+        alt=""
+        className="flex-none w-10 h-10 rounded-full"
+      />
+      <div className="flex-auto">
+        <p className="text-gray-900">{meeting.name}</p>
+        <p className="mt-0.5">
+          <time dateTime={meeting.startDatetime}>
+            {format(startDateTime, "h:mm a")}
+          </time>{" "}
+          -{" "}
+          <time dateTime={meeting.endDatetime}>
+            {format(endDateTime, "h:mm a")}
+          </time>
+        </p>
+      </div>
+      <Menu
+        as="div"
+        className="relative opacity-0 focus-within:opacity-100 group-hover:opacity-100"
+      >
+        <div>
+          <Menu.Button className="-m-2 flex items-center rounded-full p-1.5 text-gray-500 hover:text-gray-600">
+            <span className="sr-only">Open options</span>
+            <DotsVerticalIcon className="w-6 h-6" aria-hidden="true" />
+          </Menu.Button>
+        </div>
+        <Transition
+          as={Fragment}
+          enter="transition ease-out duration-100"
+          enterFrom="transform opacity-0 scale-95"
+          enterTo="transform opacity-100 scale-100"
+          leave="transition ease-in duration-75"
+          leaveFrom="transform opacity-100 scale-100"
+          leaveTo="transform opacity-0 scale-95"
+        >
+          <Menu.Items className="absolute right-0 z-10 mt-2 origin-top-right bg-white rounded-md shadow-lg w-36 ring-1 ring-black ring-opacity-5 focus:outline-none">
+            <div className="py-1">
+              <Menu.Item>
+                {({ active }) => (
+                  <a
+                    href="#"
+                    className={classNames(
+                      active ? "bg-gray-100 text-gray-900" : "text-gray-700",
+                      "block px-4 py-2 text-sm"
+                    )}
+                  >
+                    Edit
+                  </a>
+                )}
+              </Menu.Item>
+              <Menu.Item>
+                {({ active }) => (
+                  <a
+                    href="#"
+                    className={classNames(
+                      active ? "bg-gray-100 text-gray-900" : "text-gray-700",
+                      "block px-4 py-2 text-sm"
+                    )}
+                  >
+                    Cancel
+                  </a>
+                )}
+              </Menu.Item>
+            </div>
+          </Menu.Items>
+        </Transition>
+      </Menu>
+    </li>
+  );
+}
 
 export default Calendar;
